4fe746dd6b4a27256d713774839b68259d0b60ac
Erdenebulgan Sainkhishig <erdenebulgandeveloper@gmail.com>
2021-04-25 18:01:28 +0800

    add controller reset password

--- controller/users.js
+++ controller/users.js
@@ -1,9 +1,9 @@
-const User = require('../models/user');
-const asyncHandler = require('express-async-handler');
-const MyError = require('../utils/myerror');
-const paginate = require('../utils/paginate');
-const sendEmail = require('../utils/email');
-
+const User = require("../models/user");
+const asyncHandler = require("express-async-handler");
+const MyError = require("../utils/myerror");
+const paginate = require("../utils/paginate");
+const sendEmail = require("../utils/email");
+const crypto = require("crypto");
 // register
 exports.register = asyncHandler(async (req, res, next) => {
   const user = await User.create(req.body);
@@ -17,61 +17,59 @@ exports.register = asyncHandler(async (req, res, next) => {
   });
 });
 
-
-
 // login
 exports.login = asyncHandler(async (req, res, next) => {
-    const { email, password } = req.body
+  const { email, password } = req.body;
 
-    if(!email || !password) {
-        throw new MyError('Имэйл болон нууц үгээ дамжуулна уу.', 404)
-    }
+  if (!email || !password) {
+    throw new MyError("Имэйл болон нууц үгээ дамжуулна уу.", 404);
+  }
 
-    const user = await User.findOne({ email }).select('+password')
-  
-    if(!user) {
-        throw new MyError('Имэйл болон нууц үгээ зөв оруулна уу.', 401)
-    }
+  const user = await User.findOne({ email }).select("+password");
 
-    const ok = await user.checkPassword(password)
+  if (!user) {
+    throw new MyError("Имэйл болон нууц үгээ зөв оруулна уу.", 401);
+  }
 
-    if(!ok) {
-        throw new MyError('Имэйл болон нууц үгээ зөв оруулна уу.', 401)
-    }
+  const ok = await user.checkPassword(password);
 
-    res.status(200).json({
-      success: true,
-      token: user.getJWT(),
-      user: user,
-    })
-})
+  if (!ok) {
+    throw new MyError("Имэйл болон нууц үгээ зөв оруулна уу.", 401);
+  }
+
+  res.status(200).json({
+    success: true,
+    token: user.getJWT(),
+    user: user,
+  });
+});
 
 exports.getUsers = asyncHandler(async (req, res, next) => {
-    const page = parseInt(req.query.page) || 1;
-    const limit = parseInt(req.query.limit) || 10;
-    const sort = req.query.sort;
-    const select = req.query.select;
-  
-    ['select', 'sort', 'page', 'limit'].forEach((el) => delete req.query[el]);
-  
-    const pagination = await paginate(page, limit, User);
-  
-    const users = await User.find(req.query, select)
-      .sort(sort)
-      .skip(pagination.start - 1)
-      .limit(limit);
-  
-    res.status(200).json({
-      success: true,
-      data: users,
-      pagination,
-    });
+  const page = parseInt(req.query.page) || 1;
+  const limit = parseInt(req.query.limit) || 10;
+  const sort = req.query.sort;
+  const select = req.query.select;
+
+  ["select", "sort", "page", "limit"].forEach((el) => delete req.query[el]);
+
+  const pagination = await paginate(page, limit, User);
+
+  const users = await User.find(req.query, select)
+    .sort(sort)
+    .skip(pagination.start - 1)
+    .limit(limit);
+
+  res.status(200).json({
+    success: true,
+    data: users,
+    pagination,
+  });
 });
 
 exports.getUser = asyncHandler(async (req, res, next) => {
-  const user = await User.findById(req.params.id)
+  const user = await User.findById(req.params.id);
   if (!user) {
-    throw new MyError(req.params.id + ' ID-тэй хэрэглэгч байхгүй.', 400);
+    throw new MyError(req.params.id + " ID-тэй хэрэглэгч байхгүй.", 400);
   }
   res.status(200).json({
     success: true,
@@ -94,7 +92,7 @@ exports.updateUser = asyncHandler(async (req, res, next) => {
   });
 
   if (!user) {
-    throw new MyError(req.params.id + ' ID-тэй хэрэглэгч байхгүй.', 400);
+    throw new MyError(req.params.id + " ID-тэй хэрэглэгч байхгүй.", 400);
   }
 
   res.status(200).json({
@@ -108,7 +106,7 @@ exports.deleteUser = asyncHandler(async (req, res, next) => {
     const user = await User.findById(req.params.id);
 
     if (!user) {
-      throw new MyError(req.params.id + ' ID-тэй хэрэглэгч байхгүй.', 400);
+      throw new MyError(req.params.id + " ID-тэй хэрэглэгч байхгүй.", 400);
     }
 
     user.remove();
@@ -122,41 +120,77 @@ exports.deleteUser = asyncHandler(async (req, res, next) => {
   }
 });
 
-// forgot password 
+// forgot password
 exports.forgotPassword = asyncHandler(async (req, res, next) => {
+  if (!req.body.email) {
+    throw new MyError("Та нууц үг сэргээх имэйл хаягаа дамжуулна уу.", 400);
+  }
 
-    if (!req.body.email) {
-        throw new MyError('Та нууц үг сэргээх имэйл хаягаа дамжуулна уу.', 400);
-    }
+  const user = await User.findOne({ email: req.body.email });
 
-    const user = await User.findOne({ email: req.body.email });
+  if (!user) {
+    throw new MyError(req.body.email + " имэйлтэй хэрэглэгч олдсонгүй!", 400);
+  }
 
-    if (!user) {
-        throw new MyError(req.body.email + ' имэйлтэй хэрэглэгч олдсонгүй!', 400);
-    }
-  
-    // reset token generate function 
-    const resetToken = user.generatePasswordChangeToken();
-    await user.save();
-    // await user.save({ validateBeforeSave: false });
-    
-    // Имэйл илгээнэ.
+  // reset token generate function
+  const resetToken = user.generatePasswordChangeToken();
+  await user.save();
+  // await user.save({ validateBeforeSave: false });
 
-    const link = `https://amazon.mn/changepassword/${resetToken}`;
+  // Имэйл илгээнэ.
 
-    const message = `Сайн байна уу <br><br> Та нууц үг сэргээх хүсэлт илгээлээ.<br>
-                    Доорх линк дээр дарж нууц үгээ солино уу. <br><br><a target="_blank" href="${link}"></a><br><br> Өдрийг сайхан өнгөрүүлээрэй.`
+  const link = `https://amazon.mn/changepassword/${resetToken}`;
 
-    const info = await sendEmail({
-        email: user.email,
-        subject: 'Нууц үг өөрчлөх хүсэлт',
-        message: ''
-    })
+  const message = `Сайн байна уу <br><br> Та нууц үг сэргээх хүсэлт илгээлээ.<br>
+                    Доорх линк дээр дарж нууц үгээ солино уу. <br><br><a target="_blank" href="${link}"></a><br><br> Өдрийг сайхан өнгөрүүлээрэй.`;
 
-    console.log("Message sent: %s", info.messageId);
+  const info = await sendEmail({
+    email: user.email,
+    subject: "Нууц үг өөрчлөх хүсэлт",
+    message: "",
+  });
 
-    res.status(200).json({
-        success: true,
-        resetToken
-    });
+  console.log("Message sent: %s", info.messageId);
+
+  res.status(200).json({
+    success: true,
+    resetToken,
+  });
+});
+
+// reset password
+exports.resetPassword = asyncHandler(async (req, res, next) => {
+  if (!req.body.resetToken || !req.body.password) {
+    throw new MyError(
+      "Та нууц үг сэргээх token болон нууц үгээ дамжуулна уу.",
+      400
+    );
+  }
+
+  const encrypted = crypto
+    .createHash("sha256")
+    .update(req.body.resetToken)
+    .digest("hex");
+
+  const user = await User.findOne({
+    resetPasswordToken: encrypted,
+    resetPasswordExpire: { $gt: Date.now() },
+  });
+
+  if (!user) {
+    throw new MyError("Токен хүчингүй болсон байна!", 400);
+  }
+
+  user.password = req.body.password;
+  user.resetPasswordToken = undefined;
+  user.resetPasswordExpire = undefined;
+  await user.save();
+
+  const token = user.getJWT();
+
+  res.status(200).json({
+    success: true,
+    token,
+    user: user,
+  });
 });
--- routes/users.js
+++ routes/users.js
@@ -10,7 +10,8 @@ const {
     createUser, 
     updateUser, 
     deleteUser,
-    forgotPassword
+    forgotPassword,
+    resetPassword
 } = require("../controller/users");
 
 const { protect, authorize } = require("../middleware/protect")
@@ -22,6 +23,8 @@ const router = express.Router();
 router.route("/register").post(register);
 router.route("/login").post(login);
 router.route("/forgot-password").post(forgotPassword);
+router.route("/reset-password").post(resetPassword);
+
 
 router.use(protect)
 
